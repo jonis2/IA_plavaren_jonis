752d0ab98acaf5de57958412c9e59793fbb5a3f8
jonis2 <jonismajo@gmail.com>
2016-04-23 11:06:08 +0200

    kalmedar

--- web/data.php
+++ web/data.php
@@ -1,5 +1,27 @@
 <?php
-error_reporting(0);
+function connect_db() {
+	$conn = new mysqli('localhost', 'jonis2', '123456','plavaren');
+  if (! $conn) {
+      echo "err : ".$conn->connect_error();
+    return false;
+  } else {
+    return  $conn;
+  }
+}
+function get_user(){
+	if ($con = connect_db()) {
+		$query = "SELECT * FROM users"; 
+		$result = $con->query($query); 
+		if ($result) {
+			while ($row = $result->fetch_assoc()) {
+				echo '>' . $row['email'];
+			}
+    }
+	  	$con->close();
+  } else { 
+     echo "Conection error";
+  }
+}
 function hlavicka($nazov) {      // priraba hlavicku
 ?>
 <!DOCTYPE HTML>
@@ -10,6 +32,9 @@ function hlavicka($nazov) {      // priraba hlavicku
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 
   </head>
     <body>
@@ -24,6 +49,7 @@ function hlavicka($nazov) {      // priraba hlavicku
         </ul>
         </nav>
       </div>  <!-- CONTAINER -->
+      
 <?php
 }
 
--- web/index.php
+++ web/index.php
@@ -2,11 +2,52 @@
 include('data.php');
 hlavicka('Kalendár');
 ?>
-    <section>
-    <div class="container"> 
-      <h1>Kalendár</h1>
-    </div>
-    </section>
+      <section>
+        <div class="row">
+          <div class="col-sm-12 text-center">
+            <h3>Rozvrh</h3>
+          </div>
+        </div>
+          <div class="row">
+            <div class="col-md-4 col-xs-12">
+              <div class="container">
+                <div id="datepicker"></div>
+              </div>
+            </div>
+            <div class="col-md-8 col-xs-12">
+                <div id="calendar_info">
+                  <div class="alert alert-info">
+                    <strong>Info!</strong> Indicates a neutral informative change or action.
+                  </div>
+                </div>
+            </div>
+          </div>
+      </section>
+    <script>
+    function onDateSelect(value){     
+      $.ajax({
+        		  url: "get_date.php",
+        			dataType: 'html',
+        			data: "date="+value,
+        		  success: function(arg){
+                    
+                   $("#calendar_info").empty()
+                   $("#calendar_info").append("<p>"+arg+"</p>")
+        		  },
+        			error: function () { 
+                alert('Ajax error'); 
+              }
+        	});    
+    }
+    $(function() {
+      $( "#datepicker" ).datepicker({
+        todayHighlight: true,
+        onSelect: function(dateText, inst) { 
+           onDateSelect(dateText);
+        }
+      });
+    });
+    </script>
 <?php
 pata();
 ?>                                                      
